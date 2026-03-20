030cc90f2ec59fba8b4cbce5b09c86115a81938d
mateoreyes10 <150271611+mateoreyes10@users.noreply.github.com>
2026-03-20 11:22:48 -0500

    Initial commit - Spring Boot project

---
+++ src/main/java/com/fraude/FraudeDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.fraude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudeDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudeDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fraude/FraudeDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudeDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
